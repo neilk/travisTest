e6ee74bce84b6ba94e8a32d99534adbe56ca9389
Neil Kandalgaonkar <neilk@users.noreply.github.com>
2020-01-24 17:01:41 -0800

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -6,10 +6,12 @@ maketag() {
   echo "cooltag-$(date --iso-8601=s | tr -d ':-')"
 }
 
+git remote -v
+echo "changing"
 refspec="https://${GITHUB_TOKEN}@github.com/${repo}.git"
-git remote remove origin
-git remote add origin "$refspec"
+git remote add tagTarget "$refspec"
+git remote -v
 
 tag=$(maketag)
 git tag "$tag"
-git push origin "$tag"
+git push tagTarget "$tag"
