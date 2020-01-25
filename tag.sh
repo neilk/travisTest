c402443cb038bcb372cc5270413456795704f030
Neil Kandalgaonkar <neilk@users.noreply.github.com>
2020-01-24 16:50:28 -0800

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+repo="neilk/travisTest"
+
 maketag() {
   echo "cooltag-$(date --iso-8601=s | tr -d ':-')"
 }
 
 tag=$(maketag)
 git tag "$tag"
-git push origin "$tag" "https://${GITHUB_TOKEN}@github.com/neilk/travisTest.git
+git push origin "$tag" "https://${GITHUB_TOKEN}@github.com/${repo}.git"
